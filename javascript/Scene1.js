381f1a30c4c95d6e839e45c4aa3ad6604cd9a3ad
Hadi Cheaito <cheaitohadi97@gmail.com>
2023-07-15 02:23:09 +0300

    Adding collision

--- javascript/Scene1.js
+++ javascript/Scene1.js
@@ -6,6 +6,9 @@ class Scene1 extends Phaser.Scene{
     preload(){
         this.load.image("background", "../level1/waterbg.png");
         this.load.image("underground", "../underground/underBG.png");
+        this.load.image("blocks", "../underground/blocksBG.png");
+        this.load.image("row", "../underground/horizontalBlock.png");
+        this.load.image("column", "../underground/verticalBlock.png");
         this.load.image("ship1","/level1/ship-large2.png");
         this.load.image("bridge","/level1/bridge-vertical.png");
         this.load.image("player", "/level1/player.png");
--- javascript/Scene6.js
+++ javascript/Scene6.js
@@ -1,14 +1,39 @@
-class Scene6 extends Phaser.Scene{
-    constructor(){
+class Scene6 extends Phaser.Scene {
+    constructor() {
         super("lvl6");
     }
 
-create(){
-    this.background = this.add.image(0,0,"underground");
-    this.background.setOrigin(0,0);
+    create() {
+        this.background = this.add.image(0, 0, "underground");
+        this.background.setOrigin(0, 0);
 
+        this.player = this.physics.add.sprite(600 / 2 - 60, 600 / 2 + 60, "player");
+        this.player.setCollideWorldBounds(true);
 
-}
+        this.row = this.physics.add.sprite(300, 300, "row");
+        this.row.setImmovable(true); // Make the row immovable
 
+        this.curseorKeys = this.input.keyboard.createCursorKeys();
+        this.keys = this.input.keyboard.addKeys("W,A,S,D");
 
+        // Set up collision between player and row
+        this.physics.add.collider(this.player, this.row);
+
+    }
+
+    update() {
+        this.player.setVelocity(0);
+
+        if (this.keys.A.isDown) {
+            this.player.setVelocityX(-300);
+        } else if (this.keys.D.isDown) {
+            this.player.setVelocityX(300);
+        }
+
+        if (this.keys.W.isDown) {
+            this.player.setVelocityY(-300);
+        } else if (this.keys.S.isDown) {
+            this.player.setVelocityY(300);
+        }
+    }
 }
