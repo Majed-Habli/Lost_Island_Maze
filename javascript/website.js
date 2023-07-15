1d3c18d86f7e6b27963ed45b15e9eefbe20d7d79
joehaddad94 <127773834+joehaddad94@users.noreply.github.com>
2023-07-16 02:22:07 +0300

    Working on the website and the scrolling

--- javascript/website.js
+++ javascript/website.js
@@ -1,12 +1,25 @@
-let text = document.getElementById('')
-let text = document.getElementById('')
-let text = document.getElementById('')
-let text = document.getElementById('')
-let text = document.getElementById('')
-let text = document.getElementById('')
+let anchor = document.getElementById('anchor')
+let leftClouds = document.getElementById('left-clouds')
+let rightCloudsBot = document.getElementById('right-clouds-bot')
+let rightCloudsTop = document.getElementById('right-clouds-top')
+let rightIsland = document.getElementById('right-island')
+let crabLeft = document.getElementById('crab1')
+let crabRight = document.getElementById('crab2')
+console.log(rightCloudsBot)
 
 window.addEventListener('scroll', () => {
     let value = window.scrollY;
 
-    text.style.marginTop = value * 2.5 + 'px',
-}
+    anchor.style.marginTop = value * 1.9 + 'px';
+    leftClouds.style.marginRight = value * 1 + 'px';
+    rightCloudsBot.style.marginLeft = value * 0.5 + 'px';
+    rightCloudsTop.style.marginLeft = value * 1 + 'px';
+    rightIsland.style.marginLeft = value * 0.5 + 'px';
+    crabLeft.style.marginRight = value * 0.6 + 'px';
+    crabRight.style.marginLeft = value * 0.3 + 'px';
+    
+
+    // leftIsland.style.marginTop = value * 1.9 + 'px';
+    // shores.style.marginTop = value * 0.5 + 'px';
+    // rightIsland.style.marginLeft = value * 0.5 + 'px';
+});
